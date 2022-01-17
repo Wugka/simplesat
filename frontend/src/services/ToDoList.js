a749c74ececa49bb154925b9445fed717fb13a97
Wugka <61409654+Wugka@users.noreply.github.com>
2022-01-17 20:02:35 +0700

    Add files via upload

---
+++ frontend/src/services/ToDoList.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import axios from 'axios'
+
+const baseUrl = process.env.REACT_APP_SIMPLESAT_API;
+const headers = { 'Accept': 'application/json', 'Content-Type': 'application/json'}
+
+export function getToDoList(){
+    try {
+
+        var result = axios({
+            url: baseUrl + '/api/tasks',
+            headers: headers,
+            method: 'GET',
+        })
+
+        return result
+
+    } catch (err) {
+        console.log(err)
+    }
+}
